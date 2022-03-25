4c687a1374acb894e3fca6bb43e56749f20312c1
0xEval <0xeval@pm.me>
2022-03-25 12:33:15 +0800

    📝 Adding byte masking explanation to Level 13

--- src/levels/13-GatekeeperOne/GatekeeperOne.sol
+++ src/levels/13-GatekeeperOne/GatekeeperOne.sol
@@ -17,6 +17,9 @@ contract GatekeeperOne {
     modifier gateThree(bytes8 _gateKey) {
         require(uint32(uint64(_gateKey)) == uint16(uint64(_gateKey)), 'GatekeeperOne: invalid gateThree part one');
         require(uint32(uint64(_gateKey)) != uint64(_gateKey), 'GatekeeperOne: invalid gateThree part two');
+        // Solidity ˆ0.8.0 disallows direct explicit conversion from <address> to <uint16>
+        // > Explicit conversions to and from address are allowed for uint160, integer literals, bytes20 and contract types.
+        // - https://docs.soliditylang.org/en/latest/types.html?highlight=memory#address-literals
         require(uint32(uint64(_gateKey)) == uint16(uint160(tx.origin)), 'GatekeeperOne: invalid gateThree part three');
         _;
     }
--- src/test/13-GatekeeperOne.t.sol
+++ src/test/13-GatekeeperOne.t.sol
@@ -34,12 +34,56 @@ contract GatekeeperOneTest is DSTest {
         // Create GatekeeperOneAttack contract
         GatekeeperOneAttack gatekeeperOneAttack = new GatekeeperOneAttack(levelAddress);
 
+        // Bitwise Operation & Byte Masking
+
+        // +---+---+--------+
+        // | A | B | Output |
+        // +---+---+--------+
+        // | 0 | 0 |      0 |
+        // | 0 | 1 |      0 |
+        // | 1 | 0 |      0 |
+        // | 1 | 1 |      1 |
+        // +---+---+--------+
+
+        // bytes4 var_A  = 0xffffffff;
+        // bytes4 mask   = 0x0000ffff;
+        // bytes4 result = var_A & mask;
+        //               = 0x0000ffff
+
+        // Gate 3 - 1st condition:
+        // ---------------------------------------------------------
+        // uint32(uint64(_gateKey)) == uint16(uint64(_gateKey))
+        // Meaning: the last 8 bits of _gateKey should be equal to the last 4
+        // bits of _gateKey
+        //
+        //    0x???????? <- uint32(uint64(_key))
+        //  = 0x0000???? <- uint16(uint64(_gateKey)) padded by 4
+        //  ------------
+        //  & 0x0000FFFF <- mask
+
+        // Gate 3 - 2nd condition:
+        // ---------------------------------------------------------
+        // uint32(uint64(_gateKey)) != uint64(_gateKey)
+        // Meaning: the last 8 bits of _gateKey should be different from the
+        // last 16 bits of _gateKey
+        //
+        //    0x00000000???????? <- uint32(_gateKey)
+        // != 0x???????????????? <- uint64(_gateKey)
+        //  --------------------
+        //  & 0xFFFFFFFF00000000 <- mask
+        //  & 0x000000000000FFFF <- condition 3.1
+        //  = 0xFFFFFFFF0000FFFF <- final_mask
+
+        // Gate 3 - 3rd condition:
+        // ---------------------------------------------------------
+        // Same as the first condition but we use _gateKey = tx.origin
+
+        emit log_named_address('tx.origin', tx.origin);
+        emit log_named_uint('uint160(tx.origin)', uint160(tx.origin));
+        emit log_named_uint('uint16(uint160(tx.origin))', uint16(uint160(tx.origin)));
+        emit log_named_uint('uint32(uint64(tx.origin))', uint32(uint64(tx.origin)));
+
         bytes8 key = bytes8(uint64(uint160(tx.origin))) & 0xFFFFFFFF0000FFFF;
-        // View emitted values and compare them to the requires in Gatekeeper One
-        emit log_named_uint('Gate 3 all requires', uint32(uint64(key)));
-        emit log_named_uint('Gate 3 first require', uint16(uint64(key)));
-        emit log_named_uint('Gate 3 second require', uint64(key));
-        emit log_named_uint('Gate 3 third require', uint16(uint160(tx.origin)));
 
         // Loop through a until correct gas is found, use try catch to get around the revert
         for (uint256 i = 0; i <= 8191; i++) {
